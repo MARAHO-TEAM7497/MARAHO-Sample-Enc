7510db509a8287c9d66a9144672fd7db57a287b6
Jackson <jackson97010@yahoo.com.tw>
2019-03-19 13:17:32 +0800

    first commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,46 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+public class Robot extends TimedRobot {
+  Encoder Enc;
+  @Override
+
+
+  public void robotInit() {
+    Enc = new Encoder(2, 3,true);
+    Enc.setDistancePerPulse(1);
+    SmartDashboard.setDefaultNumber("Enc", 0);
+  }
+
+  @Override
+  public void autonomousInit() {
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  @Override
+  public void teleopInit() {
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    double pro = Enc.getDistance();
+    SmartDashboard.putNumber("Enc", pro);
+  }
+
+
+
+}
